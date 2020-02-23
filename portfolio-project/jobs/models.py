dff38ab27aeec473300030c414d4d08efd091607
Gowtham <47304952+tummalag@users.noreply.github.com>
2020-02-23 15:27:02 -0500

    django

--- portfolio-project/jobs/models.py
+++ portfolio-project/jobs/models.py
@@ -3,4 +3,7 @@ from django.db import models
 # Create your models here.
 class Job(models.Model):
 	image = models.ImageField(upload_to = 'images/')
-	summary = models.CharField(max_length = 64)
+	summary = models.CharField(max_length = 300)
+	#name = models.CharField(max_length = 50)
+
+	
